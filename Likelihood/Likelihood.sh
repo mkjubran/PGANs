46ac6872430a572948f581dacf1ace8da248b850
mkjubran <mjubran@birzeit.edu>
2021-04-28 12:37:19 +0300

    Ver3.5

--- Likelihood/Likelihood.sh
+++ Likelihood/Likelihood.sh
@@ -1,4 +1,5 @@
 python3 Likelihood_PGANs_v0.py \
+          --dataset          mnist \
           --ckptG            ../../presgan_lambda_G_0.0/netG_presgan_mnist_epoch_46.pth \
           --ckptE            ../../VAEncoder_E_lambda0.0_beta5_lr0.0002/netE_presgan_MNIST_epoch_99.pth \
           --ckptL            ../../Likelihood_lambda0.0_beta10_lr0.0002 \
--- Likelihood/Likelihood_PGANs_v0.py
+++ Likelihood/Likelihood_PGANs_v0.py
@@ -4,14 +4,9 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 import model_Likelihood as model
-import torchvision.transforms as transforms
 import torchvision
-import matplotlib
-from torch.utils.data import DataLoader
 from torchvision.utils import make_grid
-from utils import save_reconstructed_images, image_to_vid, save_loss_plot
 from torch.utils.tensorboard import SummaryWriter
-from tqdm import tqdm
 
 import shutil
 import os
@@ -22,6 +17,8 @@ import data
 
 
 parser = argparse.ArgumentParser()
+
+parser.add_argument('--dataset', required=True, help=' ring | mnist | stackedmnist | cifar10 ')
 parser.add_argument('--ckptG', type=str, default='', help='a given checkpoint file for generator')
 parser.add_argument('--logsigma_file', type=str, default='', help='a given file for logsigma for the generator')
 parser.add_argument('--ckptE', type=str, default='', help='a given checkpoint file for VA encoder')
@@ -46,78 +43,113 @@ parser.add_argument('--ckptL', type=str, default='', help='a given checkpoint fi
 parser.add_argument('--save_Likelihood', type=str, default='../../outputs', help='where to save Likelihood results')
 args = parser.parse_args()
 
-ckptE = args.ckptE
-
-matplotlib.style.use('ggplot')
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-##-- setup of VAE Encoder
-model = model.ConvVAE(args).to(device)
-optimizer = optim.Adam(model.parameters(), lr=args.lrE)
-
 ##-- preparing folders to save results
-if not os.path.exists(args.save_Likelihood):
+def likelihood_folders(args):
+ if not os.path.exists(args.save_Likelihood):
     os.makedirs(args.save_Likelihood)
-else:
+ else:
     shutil.rmtree(args.save_Likelihood)
     os.makedirs(args.save_Likelihood)
 
-if not os.path.exists(args.ckptL):
+ if not os.path.exists(args.ckptL):
      os.makedirs(args.ckptL)
-else:
+ else:
      shutil.rmtree(args.ckptL)
      os.makedirs(args.ckptL)
 
-
 ##-- loading and spliting datasets
-dataset = 'mnist'
-dat = data.load_data(dataset, '../../input' , args.batchSize, device=device, imgsize=args.imageSize, Ntrain=args.Ntrain, Ntest=args.Ntest)
-trainset = dat['X_train']
-testset = dat['X_test']
-trainloader=[]
-testloader=[]
-
-train_loss = []
-valid_loss = []
+def load_datasets(data,args,device):
+ dat = data.load_data(args.dataset, '../../input' , args.batchSize, device=device, imgsize=args.imageSize, Ntrain=args.Ntrain, Ntest=args.Ntest)
+ trainset = dat['X_train']
+ testset = dat['X_test']
+ return trainset, testset
 
 ##-- loading PGAN generator model
-netG = nets.Generator(args).to(device)
-netG.apply(utilsG.weights_init)
-if args.ckptG != '':
+def load_generator(nets,device):
+ netG = nets.Generator(args).to(device)
+ netG.apply(utilsG.weights_init)
+ if args.ckptG != '':
     netG.load_state_dict(torch.load(args.ckptG))
-else:
+ else:
    print('A valid ckptG for a pretrained PGAN generator must be provided')
+ return netG
 
 ##-- loading VAE encoder model
-if ckptE != '':
-        model.load_state_dict(torch.load(ckptE))
-else:
+def load_encoder(args):
+ if args.ckptE != '':
+        model.load_state_dict(torch.load(args.ckptE))
+ else:
         print('A valid ckptE for a pretrained encoder must be provided')
+ return model
 
-##-- write to tensor board
-writer = SummaryWriter(args.ckptL)
 
-##-- setting scale and selecting a random test sample
-imageSize = args.imageSize
-scale = 0.01*torch.ones(imageSize**2)
-scale = scale.to(device)
-i = torch.randint(0, len(testset),(1,1)) ## selection of the index of test image
-data = testset[i].view([1,1,imageSize,imageSize])
-data = data.to(device)
+def dist(args, mu, logvar, mean, scale, data):
+ imageSize = args.imageSize
 
-model.train()
-running_loss = 0.0
-counter = 0
-train_loss = []
-overlap_loss = 0;
-epoch = 0;
-while (epoch <= args.epochs) and (overlap_loss >= 0):
+ ##-- compute MVN full batch
+ mvn = torch.distributions.MultivariateNormal(mean, scale_tril=torch.diag(scale).reshape(1, imageSize*imageSize, imageSize*imageSize))
+ log_pxz_mvn = mvn.log_prob(data.view(-1,imageSize*imageSize))
+
+ ##-- sample from standard normal distribution
+ std = torch.exp(0.5*logvar)
+ std_b = torch.eye(std.size(1)).to(device)
+ std_c = std.unsqueeze(2).expand(*std.size(), std.size(1))
+ std_3d = std_c * std_b
+ mvnz = torch.distributions.MultivariateNormal(mu, scale_tril=std_3d)
+ pz_normal = torch.exp(mvnz.log_prob(zr))
+ return log_pxz_mvn, pz_normal
+
+
+if __name__ == "__main__":
+ ##-- run on the available GPU otherwise CPUs
+ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+ ##-- setup the VAE Encoder
+ model = model.ConvVAE(args).to(device)
+ optimizer = optim.Adam(model.parameters(), lr=args.lrE)
+
+ ##-- preparing folders to save results
+ likelihood_folders(args)
+
+ ##-- loading and spliting datasets
+ trainset, testset = load_datasets(data,args,device)
+
+ train_loss = []
+ valid_loss = []
+
+ ##-- loading PGAN generator model
+ netG = load_generator(nets,device)
+
+ ##-- loading VAE encoder model
+ model = load_encoder(args)
+
+ ##-- write to tensor board
+ writer = SummaryWriter(args.ckptL)
+
+ ##-- setting scale and selecting a random test sample
+ imageSize = args.imageSize
+ scale = 0.01*torch.ones(imageSize**2)
+ scale = scale.to(device)
+ i = torch.randint(0, len(testset),(1,1)) ## selection of the index of test image
+ data = testset[i].view([1,1,imageSize,imageSize])
+ data = data.to(device)
+
+ model.train()
+ running_loss = 0.0
+ counter = 0
+ train_loss = []
+ overlap_loss = 0;
+ epoch = 0;
+ while (epoch <= args.epochs) and (overlap_loss >= 0):
         epoch +=1
         counter += 1
         optimizer.zero_grad()
         x_hat, mu, logvar, z, zr = model(data, netG)
         mean = x_hat.view(-1,imageSize*imageSize)
 
+        log_pxz_mvn, pz_normal = dist(args, mu, logvar, mean, scale, data)
+
+        '''
         ##-- compute MVN full batch
         mvn = torch.distributions.MultivariateNormal(mean, scale_tril=torch.diag(scale).reshape(1, imageSize*imageSize, imageSize*imageSize))
         log_pxz_mvn = mvn.log_prob(data.view(-1,imageSize*imageSize))
@@ -129,6 +161,7 @@ while (epoch <= args.epochs) and (overlap_loss >= 0):
         std_3d = std_c * std_b
         mvnz = torch.distributions.MultivariateNormal(mu, scale_tril=std_3d)
         pz_normal = torch.exp(mvnz.log_prob(zr))
+        '''
 
         ##-- definning overlap loss abd backpropagation 
         overlap_loss = -1*(log_pxz_mvn + pz_normal)
@@ -150,6 +183,5 @@ while (epoch <= args.epochs) and (overlap_loss >= 0):
             img_grid_TB = torchvision.utils.make_grid(torch.cat((data, x_hat), 0).detach().cpu())
             writer.add_image('True and recon_image', img_grid_TB, epoch)
 
-writer.flush()
-writer.close()
-
+ writer.flush()
+ writer.close()
